7012440ebc1a675e4894b5c4f7d67fbaa02fd525
haribo09876 <haribo09876@gmail.com>
2024-05-09 15:19:16 +0900

    20240509
    by KYW

--- src/App.js
+++ src/App.js
@@ -4,7 +4,15 @@ import {Text, View} from 'react-native';
 export default function App() {
   return (
     <View>
-      <Text>TEST</Text>
+      <Text>TEST1</Text>
+      <Text>TEST2</Text>
+      <Text>TEST3</Text>
+      <Text>TEST4</Text>
+      <Text>TEST5</Text>
+      <Text>TEST6</Text>
+      <Text>TEST7</Text>
+      <Text>TEST8</Text>
+      <Text>TEST9</Text>
     </View>
   );
 }
