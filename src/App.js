d3a7a1e3f94f35d3c5ffa4867de2728fdf9dcb29
haribo09876 <haribo09876@gmail.com>
2024-05-09 19:57:43 +0900

    20240509
    by KYW

--- src/App.js
+++ src/App.js
@@ -1,51 +1,26 @@
-import React from 'react';
-import {ScrollView, Text, View} from 'react-native';
+import * as React from 'react';
+import {View, Text} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
-export default function App() {
+function HomeScreen() {
   return (
-    <View>
-      <ScrollView>
-        <Text>TEST1</Text>
-        <Text>TEST2</Text>
-        <Text>TEST3</Text>
-        <Text>TEST4</Text>
-        <Text>TEST5</Text>
-        <Text>TEST6</Text>
-        <Text>TEST7</Text>
-        <Text>TEST8</Text>
-        <Text>TEST9</Text>
-        <Text>TEST10</Text>
-        <Text>TEST11</Text>
-        <Text>TEST12</Text>
-        <Text>TEST13</Text>
-        <Text>TEST14</Text>
-        <Text>TEST15</Text>
-        <Text>TEST16</Text>
-        <Text>TEST17</Text>
-        <Text>TEST18</Text>
-        <Text>TEST19</Text>
-        <Text>TEST20</Text>
-        <Text>TEST21</Text>
-        <Text>TEST22</Text>
-        <Text>TEST23</Text>
-        <Text>TEST24</Text>
-        <Text>TEST25</Text>
-        <Text>TEST26</Text>
-        <Text>TEST27</Text>
-        <Text>TEST28</Text>
-        <Text>TEST29</Text>
-        <Text>TEST30</Text>
-        <Text>TEST31</Text>
-        <Text>TEST32</Text>
-        <Text>TEST33</Text>
-        <Text>TEST34</Text>
-        <Text>TEST35</Text>
-        <Text>TEST36</Text>
-        <Text>TEST37</Text>
-        <Text>TEST38</Text>
-        <Text>TEST39</Text>
-        <Text>TEST40</Text>
-      </ScrollView>
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Home Screen</Text>
     </View>
   );
 }
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
