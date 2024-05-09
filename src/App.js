f994290e670234308248dd52b83ea14f219e894a
haribo09876 <haribo09876@gmail.com>
2024-05-09 16:29:24 +0900

    20240509
    by KYW

--- src/App.js
+++ src/App.js
@@ -1,18 +1,51 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {ScrollView, Text, View} from 'react-native';
 
 export default function App() {
   return (
     <View>
-      <Text>TEST1</Text>
-      <Text>TEST2</Text>
-      <Text>TEST3</Text>
-      <Text>TEST4</Text>
-      <Text>TEST5</Text>
-      <Text>TEST6</Text>
-      <Text>TEST7</Text>
-      <Text>TEST8</Text>
-      <Text>TEST9</Text>
+      <ScrollView>
+        <Text>TEST1</Text>
+        <Text>TEST2</Text>
+        <Text>TEST3</Text>
+        <Text>TEST4</Text>
+        <Text>TEST5</Text>
+        <Text>TEST6</Text>
+        <Text>TEST7</Text>
+        <Text>TEST8</Text>
+        <Text>TEST9</Text>
+        <Text>TEST10</Text>
+        <Text>TEST11</Text>
+        <Text>TEST12</Text>
+        <Text>TEST13</Text>
+        <Text>TEST14</Text>
+        <Text>TEST15</Text>
+        <Text>TEST16</Text>
+        <Text>TEST17</Text>
+        <Text>TEST18</Text>
+        <Text>TEST19</Text>
+        <Text>TEST20</Text>
+        <Text>TEST21</Text>
+        <Text>TEST22</Text>
+        <Text>TEST23</Text>
+        <Text>TEST24</Text>
+        <Text>TEST25</Text>
+        <Text>TEST26</Text>
+        <Text>TEST27</Text>
+        <Text>TEST28</Text>
+        <Text>TEST29</Text>
+        <Text>TEST30</Text>
+        <Text>TEST31</Text>
+        <Text>TEST32</Text>
+        <Text>TEST33</Text>
+        <Text>TEST34</Text>
+        <Text>TEST35</Text>
+        <Text>TEST36</Text>
+        <Text>TEST37</Text>
+        <Text>TEST38</Text>
+        <Text>TEST39</Text>
+        <Text>TEST40</Text>
+      </ScrollView>
     </View>
   );
 }
