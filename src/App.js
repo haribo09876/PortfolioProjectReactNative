204eca523dd54342dda36dd06364c66374c838f7
haribo09876 <haribo09876@gmail.com>
2024-05-20 20:34:44 +0900

    20240520
    by KYW

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ export default function App() {
           placeholder="   아이디를 입력하세요"
         />
         <TextInput
+          secureTextEntry={true}
           style={styles.inputBox}
           placeholder="   비밀번호를 입력하세요"
         />
