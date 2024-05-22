6c38a0b2ef40fcc0d16930fd5a53630625365468
haribo09876 <haribo09876@gmail.com>
2024-05-22 20:11:45 +0900

    20240522
    by KYW

--- src/pages/SignupPage.js
+++ src/pages/SignupPage.js
@@ -1,39 +1,80 @@
-import React from 'react';
+import {useNavigation} from '@react-navigation/native';
+import {auth} from '../firebase';
+import {createUserWithEmailAndPassword, updateProfile} from 'firebase/auth';
+import React, {useState} from 'react';
 import {
   View,
   Text,
   StyleSheet,
   TextInput,
   TouchableOpacity,
+  Alert,
 } from 'react-native';
 
-function SignupPage() {
+function SignUpPage() {
+  const navigation = useNavigation();
+
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleNameChange = text => {
+    setName(text);
+  };
+
+  const handleEmailChange = text => {
+    setEmail(text);
+  };
+
+  const handlePasswordChange = text => {
+    setPassword(text);
+  };
+
+  const onSubmit = async () => {
+    if (name === '' || email === '' || password === '') {
+      Alert.alert('모든 필드를 채워주세요');
+      return;
+    }
+    try {
+      const credentials = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password,
+      );
+      await updateProfile(credentials.user, {displayName: name});
+      console.log(credentials.user);
+      Alert.alert('회원가입에 성공했습니다');
+      navigation.navigate('LoginPage');
+    } catch (error) {
+      console.error('Firebase 에러:', error);
+      Alert.alert('회원가입에 실패했습니다. 다시 시도해주세요');
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>SignupPage</Text>
-      {/* <TextInput
+      <TextInput
+        value={name}
+        onChangeText={handleNameChange}
+        placeholder="이름"
         style={styles.inputBox}
-        placeholder="아이디를 입력하세요"
-        textAlign="center"
       />
       <TextInput
-        secureTextEntry={true}
+        value={email}
+        onChangeText={handleEmailChange}
+        placeholder="이메일"
         style={styles.inputBox}
-        placeholder="비밀번호를 입력하세요"
-        textAlign="center"
       />
-      <TouchableOpacity style={styles.button}>
-        <Text style={styles.buttonText}>로그인</Text>
+      <TextInput
+        value={password}
+        onChangeText={handlePasswordChange}
+        placeholder="비밀번호"
+        style={styles.inputBox}
+        secureTextEntry={true}
+      />
+      <TouchableOpacity style={styles.button} onPress={onSubmit}>
+        <Text style={styles.buttonText}>가입하기</Text>
       </TouchableOpacity>
-      <TouchableOpacity style={[styles.button, styles.altButton]}>
-        <Text style={styles.buttonText}>Google로 로그인</Text>
-      </TouchableOpacity>
-      <TouchableOpacity style={[styles.button, styles.altButton]}>
-        <Text style={styles.buttonText}>Github로 로그인</Text>
-      </TouchableOpacity>
-      <TouchableOpacity style={[styles.button, styles.signupButton]}>
-        <Text style={styles.buttonText}>회원가입</Text>
-      </TouchableOpacity> */}
     </View>
   );
 }
@@ -41,42 +82,35 @@ function SignupPage() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'lavender',
+    backgroundColor: '#f5f5f5',
     alignItems: 'center',
     justifyContent: 'center',
-  },
-  text: {
-    fontSize: 70,
-    color: 'black',
-    textAlign: 'center',
-    fontWeight: 'bold',
   },
   inputBox: {
     width: 300,
     backgroundColor: 'white',
-    borderRadius: 10,
-    fontSize: 20,
-    marginTop: 10,
+    borderRadius: 25,
+    fontSize: 18,
+    paddingVertical: 10,
+    paddingHorizontal: 15,
+    marginVertical: 10,
+    borderWidth: 1,
+    borderColor: '#ddd',
   },
   button: {
-    backgroundColor: 'skyblue',
-    padding: 10,
-    borderRadius: 5,
-    marginTop: 10,
-    width: 300,
+    backgroundColor: '#3498db',
+    width: 250,
+    paddingVertical: 15,
+    paddingHorizontal: 30,
+    borderRadius: 25,
+    marginTop: 20,
     alignItems: 'center',
   },
   buttonText: {
     color: 'white',
-    fontSize: 20,
+    fontSize: 18,
     fontWeight: 'bold',
-  },
-  altButton: {
-    backgroundColor: 'lightblue',
-  },
-  signupButton: {
-    backgroundColor: 'orange',
   },
 });
 
-export default SignupPage;
+export default SignUpPage;
