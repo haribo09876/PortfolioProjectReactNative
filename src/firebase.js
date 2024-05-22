fdc2fbad715f9af003fbaf180b0c5975c64b2b1c
haribo09876 <haribo09876@gmail.com>
2024-05-22 15:36:19 +0900

    20240522
    by KYW

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import {initializeApp} from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyBJxWC5jWc3_LpAi8M8EWNNlYUyVEfDvKY',
+  authDomain: 'pprn-c7335.firebaseapp.com',
+  projectId: 'pprn-c7335',
+  storageBucket: 'pprn-c7335.appspot.com',
+  messagingSenderId: '204317415777',
+  appId: '1:204317415777:web:f0dfc82ed385f55d030d20',
+};
+
+const app = initializeApp(firebaseConfig);
