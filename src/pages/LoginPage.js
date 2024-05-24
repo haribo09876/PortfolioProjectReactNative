1e8a77548212a6a861d1f061f9eef6333a0d7b44
haribo09876 <haribo09876@gmail.com>
2024-05-24 15:04:02 +0900

    20240524
    by KYW

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -19,7 +19,7 @@ function LoginPage() {
     auth()
       .signInWithEmailAndPassword(email, password)
       .then(() => {
-        navigation.navigate('Home');
+        navigation.navigate('MainTabs');
       })
       .catch(error => {
         Alert.alert('로그인 실패', error.message);
@@ -53,7 +53,7 @@ function LoginPage() {
       </TouchableOpacity>
       <TouchableOpacity
         style={[styles.button, styles.signupButton]}
-        onPress={() => navigation.navigate('Signup')}>
+        onPress={() => navigation.navigate('SignupPage')}>
         <Text style={styles.buttonText}>회원가입</Text>
       </TouchableOpacity>
     </View>
