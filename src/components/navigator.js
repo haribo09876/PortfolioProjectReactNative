af528e9c65d46a2d624639b1e4b484534fcb0ed0
haribo09876 <haribo09876@gmail.com>
2024-05-26 18:50:26 +0900

    20240526
    by KYW

--- src/components/navigator.js
+++ src/components/navigator.js
@@ -1,34 +1,15 @@
 import React, {useState, useEffect} from 'react';
-import {View, ActivityIndicator, StyleSheet} from 'react-native';
+import {View, StyleSheet, TouchableOpacity, Text, Alert} from 'react-native';
 import {createStackNavigator} from '@react-navigation/stack';
-import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
 import {onAuthStateChanged} from 'firebase/auth';
-import {auth} from '../firebase';
+import {auth} from '../firebase'; // Firebase 설정을 임포트해야 합니다.
 import IntroPage from '../pages/IntroPage';
 import LoginPage from '../pages/LoginPage';
 import SignupPage from '../pages/SignupPage';
-import HomePage from '../pages/HomePage';
-import TweetPage from '../pages/TweetPage';
-import InstaPage from '../pages/InstaPage';
-import ShopPage from '../pages/ShopPage';
+import LoadingScreen from './loadingScreen';
+import MainTabs from './mainTabs';
 
 const Stack = createStackNavigator();
-const Tab = createMaterialTopTabNavigator();
-
-const LoadingScreen = () => (
-  <View style={styles.loadingContainer}>
-    <ActivityIndicator size="large" color="#0000ff" />
-  </View>
-);
-
-const MainTabs = () => (
-  <Tab.Navigator>
-    <Tab.Screen name="HomePage" component={HomePage} />
-    <Tab.Screen name="TweetPage" component={TweetPage} />
-    <Tab.Screen name="InstaPage" component={InstaPage} />
-    <Tab.Screen name="ShopPage" component={ShopPage} />
-  </Tab.Navigator>
-);
 
 const Navigator = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -48,20 +29,20 @@ const Navigator = () => {
     return <LoadingScreen />;
   }
 
+  const handleLogout = async navigation => {
+    try {
+      await auth.signOut();
+      navigation.navigate('LoginPage');
+    } catch (error) {
+      Alert.alert('로그아웃 에러', '로그아웃 중 문제가 발생했습니다.');
+    }
+  };
+
   return (
     <Stack.Navigator>
       {isLoggedIn ? (
         <>
-          <Stack.Screen
-            name="MainTabs"
-            component={MainTabs}
-            options={{headerTitle: 'PPRN'}}
-          />
-          <Stack.Screen
-            name="LoginPage"
-            component={LoginPage}
-            options={{headerShown: false}}
-          />
+          <Stack.Screen name="MainTabs" component={MainTabs} />
         </>
       ) : (
         <>
@@ -83,7 +64,34 @@ const Navigator = () => {
           <Stack.Screen
             name="MainTabs"
             component={MainTabs}
-            options={{headerShown: false}}
+            options={({navigation}) => ({
+              headerTitle: 'PPRN',
+              headerRight: () => (
+                <View style={styles.headerButtonsContainer}>
+                  <TouchableOpacity
+                    onPress={() =>
+                      Alert.alert('로그아웃', '정말 로그아웃 하시겠습니까?', [
+                        {
+                          text: 'Cancel',
+                          style: 'cancel',
+                        },
+                        {
+                          text: 'OK',
+                          onPress: () => handleLogout(navigation),
+                        },
+                      ])
+                    }
+                    style={styles.buttonContainer}>
+                    <Text style={styles.buttonText}>Log Out</Text>
+                  </TouchableOpacity>
+                  <TouchableOpacity
+                    onPress={() => alert('To User Page')}
+                    style={styles.buttonContainer}>
+                    <Text style={styles.buttonText}>User Page</Text>
+                  </TouchableOpacity>
+                </View>
+              ),
+            })}
           />
         </>
       )}
@@ -92,10 +100,16 @@ const Navigator = () => {
 };
 
 const styles = StyleSheet.create({
-  loadingContainer: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+  buttonContainer: {
+    marginRight: 10,
+  },
+  buttonText: {
+    color: 'orange',
+    fontSize: 16,
+  },
+  headerButtonsContainer: {
+    flexDirection: 'row',
+    marginRight: 10,
   },
 });
 
