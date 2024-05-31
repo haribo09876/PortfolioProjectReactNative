8bf29d058c85268f76953077d0efdb9c373339f6
haribo09876 <haribo09876@gmail.com>
2024-06-01 00:24:04 +0900

    20240601
    by KYW

--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,8 @@
 import {initializeApp} from 'firebase/app';
 import {initializeAuth, getReactNativePersistence} from 'firebase/auth';
 import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
-import {getStorage} from 'firebase/storage';
 import {getFirestore} from 'firebase/firestore';
+import {getStorage} from 'firebase/storage';
 
 const firebaseConfig = {
   apiKey: 'AIzaSyBJxWC5jWc3_LpAi8M8EWNNlYUyVEfDvKY',
@@ -13,12 +13,12 @@ const firebaseConfig = {
   appId: '1:204317415777:web:f0dfc82ed385f55d030d20',
 };
 
-const firebaseApp = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 
-const auth = initializeAuth(firebaseApp, {
+const auth = initializeAuth(app, {
   persistence: getReactNativePersistence(ReactNativeAsyncStorage),
 });
 
-export {firebaseApp, auth};
-export const storage = getStorage(firebaseApp);
-export const db = getFirestore(firebaseApp);
+export {app, auth};
+export const storage = getStorage(app);
+export const db = getFirestore(app);
