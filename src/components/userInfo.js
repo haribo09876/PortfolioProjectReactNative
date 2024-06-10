0de169957ae747310cc19e01cf9505a8bb001c82
haribo09876 <haribo09876@gmail.com>
2024-06-10 17:56:34 +0900

    20240610
    by KYW

--- src/components/userInfo.js
+++ src/components/userInfo.js
@@ -1,1 +1,21 @@
-// component
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+export default function UserInfo() {
+  return (
+    <View style={styles.content}>
+      <Text style={styles.text}>
+        userInfo userInfo userInfo userInfo userInfo userInfo userInfo userInfo
+      </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  content: {
+    flex: 0.4,
+  },
+  text: {
+    fontSize: 20,
+  },
+});
