a3f5216cac818e4680119664852f664cb3c4640f
haribo09876 <haribo09876@gmail.com>
2024-06-28 11:07:01 +0900

    20240628
    by KYW

--- src/components/vpn.js
+++ src/components/vpn.js
@@ -1,138 +1,46 @@
 import React, {useState, useEffect} from 'react';
-import {
-  View,
-  Text,
-  StyleSheet,
-  TouchableOpacity,
-  ActivityIndicator,
-  Alert,
-} from 'react-native';
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
-import {
-  saveConfig,
-  prepare,
-  connect,
-  disconnect,
-  getCurrentState,
-  VpnState,
-  CharonErrorState,
-  onStateChangedListener,
-  removeOnStateChangeListener,
-} from 'react-native-vpn-ipsec';
+import {View, Text, TouchableOpacity, StyleSheet} from 'react-native';
+import {prepare, connect} from 'react-native-vpn-ipsec';
 
 const Vpn = () => {
-  const [vpnConnected, setVpnConnected] = useState(false);
-  const [loading, setLoading] = useState(false);
-  const [currentState, setCurrentState] = useState(VpnState.disconnected);
+  const [isConnected, setIsConnected] = useState(false);
+  const address = '219.100.37.109';
+  const username = 'vpn';
+  const password = 'vpn';
 
-  // VPN 연결 준비
   useEffect(() => {
-    const setupVpn = async () => {
+    const initializeVpn = async () => {
       try {
         await prepare();
-        console.log('VPN 준비 완료');
+        console.log('VPN prepared successfully');
       } catch (error) {
-        console.error('VPN 준비 중 오류 발생:', error);
+        console.error('Error preparing VPN', error);
       }
     };
 
-    setupVpn();
+    initializeVpn();
   }, []);
 
-  // VPN 상태 변경 리스너 등록
-  useEffect(() => {
-    const stateChangeListener = onStateChangedListener(({state}) => {
-      setCurrentState(state);
-    });
-
-    return () => {
-      removeOnStateChangeListener(stateChangeListener);
-    };
-  }, []);
-
-  const toggleVpnConnection = async () => {
-    setLoading(true);
-
+  const handleConnect = async () => {
     try {
-      if (vpnConnected) {
-        // VPN 연결 해제
-        await disconnect();
-        setVpnConnected(false);
-      } else {
-        // VPN 연결 설정
-        const vpnConfig = {
-          address: 'public-vpn-98.opengw.net',
-          username: 'vpn',
-          password: 'vpn',
-          vpnType: undefined, // Currently not implemented
-          mtu: undefined, // Currently not implemented
-        };
-
-        await saveConfig(
-          'MyVPN',
-          vpnConfig.address,
-          vpnConfig.username,
-          vpnConfig.password,
-          vpnConfig.password,
-        );
-        await connect(
-          'MyVPN',
-          vpnConfig.address,
-          vpnConfig.username,
-          vpnConfig.password,
-          vpnConfig.vpnType,
-          vpnConfig.mtu,
-        );
-        setVpnConnected(true);
-      }
+      await connect(address, username, password);
+      setIsConnected(true);
+      console.log('VPN connected successfully');
     } catch (error) {
-      Alert.alert('오류 발생', error.message);
-    } finally {
-      setLoading(false);
+      console.error('Error connecting to VPN', error);
     }
   };
 
   return (
     <View style={styles.container}>
-      <View style={styles.header}>
-        <Text style={styles.headerText}>VPN 상태</Text>
-      </View>
-      <View style={styles.content}>
-        <Icon
-          name={vpnConnected ? 'shield-check' : 'shield-outline'}
-          size={100}
-          color={vpnConnected ? 'green' : 'lightgrey'}
-        />
-        <Text
-          style={[
-            styles.statusText,
-            {color: vpnConnected ? 'green' : 'white'},
-          ]}>
-          {vpnConnected ? '연결됨' : '연결 안 됨'}
+      <TouchableOpacity onPress={handleConnect} style={styles.button}>
+        <Text style={styles.buttonText}>
+          {isConnected ? '연결됨' : '연결하기'}
         </Text>
-        <TouchableOpacity
-          style={[
-            styles.toggleButton,
-            vpnConnected
-              ? {backgroundColor: '#ff4d4d'}
-              : {backgroundColor: '#007bff'},
-            loading && {backgroundColor: '#ccc'},
-          ]}
-          onPress={toggleVpnConnection}
-          disabled={loading}>
-          {loading ? (
-            <ActivityIndicator color="white" />
-          ) : (
-            <Text style={styles.buttonText}>
-              {vpnConnected ? '연결 해제' : '연결하기'}
-            </Text>
-          )}
-        </TouchableOpacity>
-        <Text style={styles.currentStateText}>
-          현재 상태:{' '}
-          {currentState === VpnState.connected ? '연결됨' : '연결 안 됨'}
-        </Text>
-      </View>
+      </TouchableOpacity>
+      {isConnected && (
+        <Text style={styles.statusText}>VPN에 연결되었습니다.</Text>
+      )}
     </View>
   );
 };
@@ -140,41 +48,21 @@ const Vpn = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
     justifyContent: 'center',
-    backgroundColor: '#A9A9F5',
-  },
-  header: {
-    marginBottom: 10,
-  },
-  headerText: {
-    fontSize: 30,
-    fontWeight: 'bold',
-    color: 'white',
-  },
-  content: {
     alignItems: 'center',
   },
-  statusText: {
-    fontSize: 20,
-    marginVertical: 20,
-    color: 'white',
-  },
-  currentStateText: {
-    marginTop: 20,
-    color: 'white',
-  },
-  toggleButton: {
-    paddingVertical: 10,
-    paddingHorizontal: 20,
-    borderRadius: 10,
-    marginTop: 10,
-    alignItems: 'center',
+  button: {
+    backgroundColor: '#4CAF50',
+    padding: 10,
+    borderRadius: 5,
   },
   buttonText: {
     color: 'white',
     fontSize: 18,
-    fontWeight: '500',
+  },
+  statusText: {
+    marginTop: 20,
+    fontSize: 16,
   },
 });
 
